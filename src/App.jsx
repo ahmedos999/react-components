1e75d2d458e0627e401fa2245730ea4e823426c1
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-09-24 03:28:38 +0400

    add the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ function App() {
     className='w-screen h-3/6 custom-shadow m-0 bg flex justify-center'>
       <img src={img} alt="" className='h-1/4 flex' />
     </div>
-  <div className="w-screen h-2/6 bg-black">
-  <button className=''></button>
+  <div className="w-screen h-2/6 bg-black flex flex-col justify-center items-center">
+  <button className='w-5/6 py-2 bg-blue-600 my-2'>pay the bill</button>
+  <button className='w-5/6 py-2 bg-red-400'>view the menu</button>
   </div>
 </div>
 
